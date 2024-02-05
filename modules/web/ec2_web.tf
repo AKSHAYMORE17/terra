0358ecfc10af5192677de3527d4b29f939151b22
Kamblesuraj432 <140583784+kamblesuraj432@users.noreply.github.com>
2024-02-05 11:46:05 +0530

    Add files via upload

---
+++ modules/web/ec2_web.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web-ec2-1" {
+  ami           = var.web_ec2_ami_id
+  instance_type = var.web_ec2_instance_type
+  key_name = "terraform"
+  vpc_security_group_ids = [aws_security_group.web-ec2-sg.id]
+  subnet_id = var.private_sub1_id
+  tags = {
+        Name  = "Web-Server1"
+    }
+}
